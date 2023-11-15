110fedca0aecd475eb607e6b02a4427f0a7b8c57
William Spriggs <Willspri@gmail.com>
2023-11-16 09:49:22 +1300

    matplotlib added

--- PDFReaderSimple/PDFReaderSimple.py
+++ PDFReaderSimple/PDFReaderSimple.py
@@ -1,4 +1,12 @@
 import PyPDF2
+import matplotlib.pyplot as plt
+import numpy as np
+
+#xpoints = np.array([0, 6])
+#ypoints = np.array([0, 250])
+
+#plt.plot(xpoints, ypoints)
+#plt.show()
 
 def ExtractText(pdf_file_path: str) -> str:
 
