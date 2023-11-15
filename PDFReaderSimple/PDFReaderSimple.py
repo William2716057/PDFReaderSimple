7e89b02ec28eedb77ebcd7358f7b4813dac8b9ab
William Spriggs <Willspri@gmail.com>
2023-11-16 09:39:40 +1300

    Changed

--- PDFReaderSimple/PDFReaderSimple.py
+++ PDFReaderSimple/PDFReaderSimple.py
@@ -4,14 +4,14 @@ def ExtractText(pdf_file_path: str) -> str:
 
     with open(pdf_file_path, 'rb') as file_object:
         PDFRead = PyPDF2.PdfReader(file_object)
-        total_text = ""
+        Total = ""
 
         for page_num in range(len(PDFRead.pages)):
             page = PDFRead.pages[page_num]
-            text_content = page.extract_text()
-            total_text += text_content
+            Content = page.extract_text()
+            Total += Content
 
-        return total_text
+        return Total
 
 if __name__ == '__main__':
     pdf_text = ExtractText('sample.pdf')
